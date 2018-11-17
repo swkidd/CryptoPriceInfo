b9c183e67972eb77922bf648b1a95c8e473c5148
Steven Kidd <skidd@peakey.com>
2018-11-17 15:45:45 -0500

    k

--- src/main/client/src/app/app.component.ts
+++ src/main/client/src/app/app.component.ts
@@ -1,19 +1,20 @@
-import { Component } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'IM A ROBOT!';
-  result = '';
+  @ViewChild('card') card: ElementRef;
+  from : string = "BTC";
+  to : string[] = ["USD", "GBP"];
 
-  constructor(private http : HttpClient ) {}
+  onKeyUpFrom(event) {
+    
+  }
 
-  private hello(): void {
-    this.result = 'loading...';
-    this.http.get('/api/hello').subscribe(response => this.result = response["success"]);
+  onKeyUpTo(event) {
+
   }
 }
